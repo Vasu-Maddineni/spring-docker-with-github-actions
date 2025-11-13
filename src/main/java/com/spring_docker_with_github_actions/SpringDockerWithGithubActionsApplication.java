c5ddb446cc254bbf5f31a1baefb15a39f1b9999f
Vasu-Maddineni <vasu.maddineni9226@gmail.com>
2025-11-13 15:33:25 +0530

    First Commit

---
+++ src/main/java/com/spring_docker_with_github_actions/SpringDockerWithGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_docker_with_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerWithGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerWithGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_docker_with_github_actions/SpringDockerWithGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_docker_with_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerWithGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
