45f668d9827ffe95b970f766eb70bf2dcd94edf7
Vasu-Maddineni <vasu.maddineni9226@gmail.com>
2025-11-13 16:03:23 +0530

    Added

--- src/main/java/com/spring_docker_with_github_actions/SpringDockerWithGithubActionsApplication.java
+++ src/main/java/com/spring_docker_with_github_actions/SpringDockerWithGithubActionsApplication.java
@@ -2,9 +2,17 @@ package com.spring_docker_with_github_actions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDockerWithGithubActionsApplication {
+	
+	@GetMapping
+	public String home() {
+		return "welcome to git hub auto deploy";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerWithGithubActionsApplication.class, args);
